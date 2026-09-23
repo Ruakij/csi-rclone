a95d73d36735cb3c3b641e02d0952f7fe803e3a1
Ruakij <ruakij@ruekov.eu>
2026-09-24 00:18:44 +0200

    Mount read-only for readOnly, reader-only access modes and ro mount option

---
+++ pkg/rclone/nodeserver_test.go
@@ -0,0 +1,32 @@
+package rclone
+
+import (
+	"testing"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+)
+
+func TestIsReadOnly(t *testing.T) {
+	capability := func(mode csi.VolumeCapability_AccessMode_Mode, flags ...string) *csi.VolumeCapability {
+		return &csi.VolumeCapability{
+			AccessMode: &csi.VolumeCapability_AccessMode{Mode: mode},
+			AccessType: &csi.VolumeCapability_Mount{Mount: &csi.VolumeCapability_MountVolume{MountFlags: flags}},
+		}
+	}
+	rwx := csi.VolumeCapability_AccessMode_MULTI_NODE_MULTI_WRITER
+	for _, tc := range []struct {
+		req  *csi.NodePublishVolumeRequest
+		want bool
+	}{
+		{&csi.NodePublishVolumeRequest{VolumeCapability: capability(rwx)}, false},
+		{&csi.NodePublishVolumeRequest{VolumeCapability: capability(rwx, "noatime")}, false},
+		{&csi.NodePublishVolumeRequest{VolumeCapability: capability(rwx), Readonly: true}, true},
+		{&csi.NodePublishVolumeRequest{VolumeCapability: capability(rwx, "ro")}, true},
+		{&csi.NodePublishVolumeRequest{VolumeCapability: capability(csi.VolumeCapability_AccessMode_MULTI_NODE_READER_ONLY)}, true},
+		{&csi.NodePublishVolumeRequest{VolumeCapability: capability(csi.VolumeCapability_AccessMode_SINGLE_NODE_READER_ONLY)}, true},
+	} {
+		if got := isReadOnly(tc.req); got != tc.want {
+			t.Errorf("isReadOnly(%v) = %v, want %v", tc.req, got, tc.want)
+		}
+	}
+}
